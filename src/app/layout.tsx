df68efd0272830b250f0abdd8fef8619fd2f987c
lianaa98 <liana.kan98@gmail.com>
2023-08-01 12:32:39 -0700

    minor styling for landing page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,12 +16,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} 
+      <body className={`
+      ${inter.className} 
       bg-slate-800
       text-slate-100
-      container
-      mx-auto
-      p-4`}>{children}</body>
+      `}>{children}</body>
     </html>
   )
 }
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -3,11 +3,11 @@ import axios from "axios";
 export const verifyAuth = async (jwt: string) => {
   try {
     console.log("verifying auth...")
-    
+
     const BASE_URL = process.env.BASE_URL;
     console.log("BASE_URL", BASE_URL)
 
-    const verified = await axios.get(`http://localhost:8080/verifyUser`, {
+    const verified = await axios.get(`${BASE_URL}/verifyUser`, {
       headers: {
         Authorization: jwt,
       },
