e751aa95ef76055f16e4c71b43e6418fd9f66f2c
lianaa98 <liana.kan98@gmail.com>
2023-08-24 15:23:50 -0700

    displaying fetched item data on modal

--- src/app/dashboard/components/ItemModal.tsx
+++ src/app/dashboard/components/ItemModal.tsx
@@ -13,7 +13,24 @@ interface ItemModalProps {
   viewModalData: OwnedItem | null;
 }
 
-const ItemModal: FC<ItemModalProps> = ({ setOpenViewModal, viewModalLoading, viewModalData }) => {
+const ItemModal: FC<ItemModalProps> = ({
+  setOpenViewModal,
+  viewModalLoading,
+  viewModalData,
+}) => {
+  const {
+    id,
+    name,
+    brand,
+    description,
+    quantity,
+    created_at,
+    updated_at,
+    purchased_at,
+    status,
+    location,
+    user,
+  } = viewModalData;
   return (
     <Modal
       open
@@ -22,16 +39,26 @@ const ItemModal: FC<ItemModalProps> = ({ setOpenViewModal, viewModalLoading, vie
       aria-describedby="modal-modal-description"
     >
       <Box className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-slate-700 rounded-md p-7">
-        <div>
-          <div>
-            <span>Image</span>
+        <div className="flex">
+          <div className="flex flex-col mx-2">
+            <span>Name: {name}</span>
+            <span>Brand: {brand}</span>
+            <span>Quantity: {quantity}</span>
           </div>
-          <div>
-            <span>info</span>
+          <div className="flex flex-col mx-2">
+            <div className="bg-orange-300 rounded-sm text-center">
+              <span>{status}</span>
+            </div>
+            <div className="bg-slate-900 rounded-md p-5">
+              <span>{description}</span>
+            </div>
           </div>
         </div>
-        <div>
-          <span>buttons</span>
+        <div className="mx-2">
+          <button>Edit</button>
+          <button>Move</button>
+          <button>Add to grocery list</button>
+          <button>Delete</button>
         </div>
       </Box>
     </Modal>
